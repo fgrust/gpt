13405f72571a29034cb6e0b810e5309867769eb7
fgrust <freezy.gem0918@outlook.com>
2023-06-23 05:35:43 -0400

    test my macro

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod test_my_macro;
+mod test_rc;
+mod test_concurrency;
+
+fn main() {
+    println!("Hello, world!");
+}
+
---
+++ src/test_my_macro.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod tests {
+    use my_proc_macro::function_to_string;
+
+    const OUTPUT: &str = "";
+
+    #[function_to_string]
+    fn some_function_for_ai_llm(_whatever_params: &str) {
+        /// This is an awesome function
+        /// We shall give it to an AI to guess and output
+        /// In a structured manner
+        println!("{}", OUTPUT);
+    }
+
+    #[test]
+    fn tests_proc_macro() {
+        let x: &str = some_function_for_ai_llm("some_input");
+        dbg!(x);
+    }
+}
